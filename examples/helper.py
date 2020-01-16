fceeea49e10b1a9a29286efafc8301cdf2fc7ab3
mesnardo <mesnardo@gwu.edu>
2020-01-16 16:16:02 -0500

    Revise examples with new functions

--- examples/helper.py
+++ examples/helper.py
@@ -3,59 +3,8 @@
 from matplotlib import pyplot
 import numpy
 from scipy.sparse import coo_matrix
-from scipy.sparse.linalg import eigsh
 
 import pyibm
-
-
-def is_symmetric(Op, tol=1e-12):
-    A = Op - Op.T
-    A = A.multiply(abs(A) > tol)
-    return A.nnz == 0
-
-
-def condition_number(Op):
-    evals_large, _ = eigsh(Op, 1, which='LM')
-    evals_small, _ = eigsh(Op, 1, sigma=0, which='LM')
-    lambda_min, lambda_max = evals_small[0], evals_large[0]
-    cond = lambda_max / lambda_min
-    return lambda_min, lambda_max, cond
-
-
-def print_matrix_info(Op):
-    """Print information of a given sparse matrix."""
-    print('Type: ', type(Op))
-    print('Shape: ', Op.shape)
-    print('Size: ', Op.data.size)
-    if Op.data.size > 0:
-        print('Min/Max: ', Op.data.min(), Op.data.max())
-
-
-def plot_matrix(M, figsize=(6.0, 6.0), axis_scaled=True,
-                markersize=1, color='red', cmap=None,
-                limits=[None, None, None, None]):
-    """Plot non-zero structure of an operator."""
-    if not isinstance(M, coo_matrix):
-        M = coo_matrix(M)
-    pyplot.rc('font', family='serif', size=16)
-    fig, ax = pyplot.subplots(figsize=figsize)
-    if cmap is None:
-        ax.scatter(M.col, M.row, c=color, s=markersize, marker='s')
-    else:
-        sc = ax.scatter(M.col, M.row, c=M.data, s=markersize, cmap=cmap)
-        fig.colorbar(sc)
-    if axis_scaled:
-        ax.axis('scaled', adjustable='box')
-    default_limits = [0, M.shape[0] - 1, 0, M.shape[1] - 1]
-    for i, lim in enumerate(limits):
-        if lim is None:
-            limits[i] = default_limits[i]
-    ax.set_xlim(limits[2:])
-    ax.set_ylim(limits[:2])
-    ax.invert_yaxis()
-    ax.set_xticks([])
-    ax.set_yticks([])
-    return fig, ax
 
 
 def plot_contourf(field, grid, body=None,
