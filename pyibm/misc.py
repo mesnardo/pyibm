171e40b34bcb37ac80de87708f627b957e361188
mesnardo <mesnardo@gwu.edu>
2020-01-16 12:39:23 -0500

    Add routine to estimate condition number

--- pyibm/misc.py
+++ pyibm/misc.py
@@ -1,6 +1,32 @@
 """Implementation of miscellaneous functions."""
 
 import numba
+from scipy.sparse.linalg import eigsh
+
+
+def condition_number(A):
+    """Estimate the smallest and largest eigenvalues, and condition number.
+
+    Parameters
+    ----------
+    A : scipy.sparse.csr_matrix
+        The sparse operator.
+
+    Returns
+    -------
+    float
+        Estimation of the smallest eigenvalue.
+    float
+        Estimation of the largest eigenvalue.
+    float
+        Condition number (ratio max/min).
+
+    """
+    evals_large, _ = eigsh(A, 1, which='LM')
+    evals_small, _ = eigsh(A, 1, sigma=0, which='LM')
+    lambda_min, lambda_max = evals_small[0], evals_large[0]
+    cond = lambda_max / lambda_min
+    return lambda_min, lambda_max, cond
 
 
 @numba.njit
