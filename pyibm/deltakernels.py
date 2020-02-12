6554cd8f12c638a92015a13618497ee5f53b3970
mesnardo <mesnardo@gwu.edu>
2020-02-11 20:29:06 -0500

    Add kernel for Stockie (1997) delta function

--- pyibm/deltakernels.py
+++ pyibm/deltakernels.py
@@ -56,6 +56,38 @@ def delta_peskin_2002(r, dr):
 
 
 @numba.njit
+def delta_stockie_1997(r, dr):
+    """Compute the discrete delta function from Stockie (1997).
+
+    Parameters
+    ----------
+    r : float
+        Directional distance between the source and target.
+    dr : float
+        Grid-spacing of the underlying Cartesian mesh.
+
+    Returns
+    -------
+    Value of the discrete delta function.
+
+    """
+    x = r / dr
+    if 0.0 <= x < 1.0:
+        return (61 / 112 - 11 / 42 * x - 11 / 56 * x**2 + 1 / 12 * x**3 +
+                math.sqrt(3) / 336 *
+                math.sqrt(243 + 1584 * x - 748 * x**2 - 1560 * x**3 +
+                          500 * x**4 + 336 * x**5 - 112 * x**6)) / dr
+    elif 1.0 <= x < 2.0:
+        return (21 / 16 + 7 / 12 * x - 7 / 8 * x**2 + 1 / 6 * x**3 -
+                3 / 2 * delta_stockie_1997(r - 1, dr)) / dr
+    elif 2.0 <= x < 3:
+        return (9 / 8 - 23 / 12 * x + 3 / 4 * x**2 - 1 / 12 * x**3 +
+                1 / 2 * delta_stockie_1997(r - 2, dr)) / dr
+    else:
+        return 0.0
+
+
+@numba.njit
 def delta(r, dr, kernel=delta_roma_et_al_1999):
     """Compute the n-dimensional product of the discrete delta function.
 
