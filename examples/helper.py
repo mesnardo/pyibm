1bd2edd49b10b3cbcbdafb1ea1695c9962d33d0f
mesnardo <mesnardo@gwu.edu>
2020-02-21 10:14:00 -0500

    Fix bug in boundary velocity and revise TGV notebook

--- examples/helper.py
+++ examples/helper.py
@@ -1,5 +1,6 @@
 """Helper functions."""
 
+import math
 from matplotlib import pyplot
 import numpy
 from scipy.sparse import coo_matrix
@@ -7,12 +8,12 @@ from scipy.sparse import coo_matrix
 import pyibm
 
 
-def plot_contourf(field, grid, body=None,
+def plot_contourf(field, grid,
+                  body=None, body_plot_kwargs={},
                   levels=None,
                   axis_lim=(None, None, None, None),
-                  show_grid=False):
+                  show_grid=False, grid_scatter_kwargs={}):
     """Plot the filled contour of the 2D field."""
-    pyplot.rc('font', family='serif', size=16)
     fig, ax = pyplot.subplots(figsize=(8.0, 8.0))
     X, Y = numpy.meshgrid(grid.x.vertices, grid.y.vertices)
     if show_grid:
@@ -22,17 +23,40 @@ def plot_contourf(field, grid, body=None,
             ax.axvline(xi, ymin=ymin, ymax=ymax, color='grey')
         for yi in grid.y.vertices:
             ax.axhline(yi, xmin=xmin, xmax=xmax, color='grey')
-        ax.scatter(X, Y, marker='x')
+        ax.scatter(X, Y, **grid_scatter_kwargs)
     if body is not None:
-        ax.scatter(body.x, body.y, color='C3')
+        ax.plot(body.x, body.y, **body_plot_kwargs)
     if levels is None:
         levels = numpy.linspace(numpy.min(field), numpy.max(field), num=51)
+    if field.ndim == 1:
+        field = field.reshape(grid.shape)
     contf = ax.contourf(X, Y, field,
                         levels=levels, extend='both', zorder=0)
     fig.colorbar(contf)
     ax.axis('scaled', adjustable='box')
     ax.axis(axis_lim)
     return fig, ax
+
+
+def get_sub_field(u, grid, box=(-numpy.infty, numpy.infty,
+                                -numpy.infty, numpy.infty)):
+    """Return the solution of u inside a given box."""
+    if u.ndim == 1:
+        u = u.reshape(grid.shape)
+    x, y = grid.x.vertices, grid.y.vertices
+    xlim, ylim = box[:2], box[2:]
+    mask_x = numpy.where((x >= xlim[0]) & (x <= xlim[1]))[0]
+    mask_y = numpy.where((y >= ylim[0]) & (y <= ylim[1]))[0]
+    return (x[mask_x], y[mask_y]), u[mask_y[0]:mask_y[-1] + 1,
+                                     mask_x[0]:mask_x[-1] + 1]
+
+
+def print_stats(u):
+    """Print statistics about a given array."""
+    print(f'Mean: {numpy.mean(u)}')
+    print(f'Min, Max: {numpy.min(u)}, {numpy.max(u)}')
+    print(f'L_inf: {numpy.max(numpy.abs(u))}')
+    print(f'L_2: {numpy.linalg.norm(u, ord=None)}')
 
 
 def plot_grids(grid, body=None, Op=None,
@@ -79,3 +103,29 @@ def plot_grids(grid, body=None, Op=None,
     ax.axis('scaled', adjustable='box')
     ax.axis(axis_lim)
     return fig, ax
+
+
+def taylor_green_vortex(x, y, t, nu, dim=2):
+    """Return the solution of the Taylor-Green vortex at given time."""
+    if dim == 2:
+        X, Y = numpy.meshgrid(x, y)
+    else:
+        X, Y = x, y
+    a = 2 * numpy.pi
+    u = -numpy.cos(a * X) * numpy.sin(a * Y) * numpy.exp(-2 * a**2 * nu * t)
+    v = +numpy.sin(a * X) * numpy.cos(a * Y) * numpy.exp(-2 * a**2 * nu * t)
+    p = (-0.25 * (numpy.cos(2 * a * X) + numpy.cos(2 * a * Y)) *
+         numpy.exp(-4 * a**2 * nu * t))
+    return u, v, p
+
+
+def circle(R=0.5, center=(0.0, 0.0), N=None, ds=None):
+    """Create the coordinates along a circle using a uniform distribution."""
+    if N is None:
+        if ds is None:
+            raise ValueError('Provide argument N or ds')
+        N = math.ceil(2 * math.pi * R / ds)
+    theta = numpy.linspace(0.0, 2 * numpy.pi, num=N + 1)[:-1]
+    x = center[0] + R * numpy.cos(theta)
+    y = center[1] + R * numpy.sin(theta)
+    return x, y
